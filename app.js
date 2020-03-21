f23a69485a45c86abbf0c779adbcb493e21d0de6
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-03-21 20:33:18 +0530

    Add first template for four cards

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cwd = process.cwd();
 const config = require(path.join(cwd, '/config/config'));
-
+const viewsDirectory = path.join(cwd, '/src/public/views');
 let options = {};
 if (config.NODE_ENV !== 'production') {
     options.overrideProcessEnv = true;
@@ -14,8 +14,22 @@ const port = config.port;
 const app = express();
 app.use(express.static(path.join(cwd, '/src/public')));
 
+app.set('views', viewsDirectory);
+app.set('view engine', 'ejs');
+
 app.get('/', (request, response) => {
-    response.send("Hello from Server");
+});
+
+app.get('/four-cards', (request, response) => {
+    response.render('four-cards');
+});
+
+app.get('/signup-form', (request, response) => {
+    response.render('signup-form');
+});
+
+app.get('/pricing-cards', (request, response) => {
+    response.render('pricing-cards');
 });
 
 app.listen(port, () => {
