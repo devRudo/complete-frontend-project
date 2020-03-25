56b25cc80ccac9a4fa4fce9fe70e5058a7442070
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-03-25 17:18:46 +0530

    fix pricing cards mode change issue on mobile

--- src/public/js/pricing-cards.js
+++ src/public/js/pricing-cards.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("annually").style.display = "flex";
     document.getElementById("monthly").style.display = "none";
-    document.getElementById('mode').addEventListener('click', () => {
+    document.getElementById('mode').addEventListener('change', () => {
+        console.log(document.getElementById('mode').value);
         if (document.getElementById('mode').value == 0) {
             document.getElementById("annually").style.display = "flex";
             document.getElementById("monthly").style.display = "none";
