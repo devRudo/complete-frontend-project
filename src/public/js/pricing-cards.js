1a56283ccf0da44b0db4e7f14d71f0a1765b35b1
VIJAY KUMAR MISHRA <vijay.mishra@mountblue.tech>
2020-03-25 18:17:18 +0530

    fix Pricing cards click to active

--- src/public/js/pricing-cards.js
+++ src/public/js/pricing-cards.js
@@ -12,4 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById("monthly").style.display = "flex";
         }
     });
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', function () {
+            let siblings = [...cards[i].parentElement.children];
+            siblings = siblings.filter((elem) => elem.classList.contains('card-active'));
+            siblings[0].classList.remove('card-active');
+            cards[i].classList.add('card-active');
+            if (cards[i].classList.contains('basic')) {
+                cards[i].style.borderTopRightRadius = '8px';
+                cards[i].style.borderBottomRightRadius = '8px';
+            }
+            if (cards[i].classList.contains('master')) {
+                cards[i].style.borderTopLeftRadius = '8px';
+                cards[i].style.borderBottomLeftRadius = '8px';
+            }
+        });
+    }
 });
